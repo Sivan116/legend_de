e5b2bff7b018a7bd0000f23e72efad705921158d
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 10:52:29 +0300

    Add authenticateUser function

--- services/UsersService.js
+++ services/UsersService.js
@@ -1,20 +1,32 @@
 const pool =  require('../db/config');
+const bcrypt = require ('bcrypt');
 
 const getAll = async () => {
     return await pool.query('SELECT * FROM t_users')
                         .then(res => { return res.rows; });
 }
 
-const findUser = async (username, password) => {
+const authenticateUser = async (username, password) => {
     const query = {
         name: 'fetch-user',
-        text: 'SELECT * FROM t_users WHERE username = $1 AND password = $2',
-        values: [username, password],
-      }
+        text: 'SELECT * FROM t_users WHERE username = $1',
+        values: [username],
+    }
 
-    return await pool.query(query).then(res => { return res.rows[0]; });
+    const User = await pool.query(query).then(res => { return res.rows[0]; });
+    if(User) {
+        bcrypt.compare(password, User.password, function(err, result) {
+            if (result) {
+              return true;
+            }
+            else {
+              return false;
+            }
+        });
+    }
 }
 
 module.exports = {
     getAll,
+    authenticateUser,
 };
