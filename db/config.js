f3d5d21182cebd3fdff8b4b6d99dd50a760cad29
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 09:11:59 +0300

    Changed DB login credentials

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://asd:asd@172.30.210.131:5432/postgres`;
+const connectionString = `postgresql://asd:asd@172.30.130.177:5432/postgres`;
 
                           // postgres://username:password@host:port/database
 const pool = new Pool({
