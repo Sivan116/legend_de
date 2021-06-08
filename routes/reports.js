2ffc1bc5759d3fb5c07adb0bcd6063732a78d314
shkeepr <education8090@gmail.com>
2021-06-08 15:34:56 +0300

    Revert "Merge branch 'luda/secureAPI' into dev"
    This reverts commit 7fe74e591da6b68b7c7831c59c40b50dc54ab7bf, reversing
    changes made to dcb5b916e7f9b6bb4962fb87b9d8eb3e5923e4be.

--- routes/reports.js
+++ routes/reports.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const weatherService = require('../services/WeatherService');
+const reportsService = require('../services/SecurityReportsService');
 
-router.post('/', async (req, res) => {
-    const { weather } = req.body;
-    res.send(await weatherService.updateWeather(weather));
-});
-
-router.get('/weather', async (req, res) => {
-    return res.json(await weatherService.getWeatherForecast());
-});
-
-router.get('/calendar', async (req, res) => {
-    return res.json(await weatherService.getWeatherForecast());
+router.get('/', function(req, res, next) {
+   res.send(reportsService.getReports());
 });
 
 module.exports = router;
--- services/SecurityReportsService.js
+++ services/SecurityReportsService.js
@@ -1,48 +1,20 @@
-const axios = require('axios');
 const pool =  require('../db/config');
-const format = require('pg-format');
 
-const getReports = async () => {
-    axios.get('')
-  .then(response => {
-    const parsedData = parseReports(response.data);
-
-    return JSON.stringify(parsedData);
-  })
-  .catch(error => {
-    return await pool.query('SELECT * FROM t_reports')
-                        .then(res => { return res.rows; });
-  });
+const getAll = () => {
+    return reports = [
+        {
+            ev_type: 'Theft',
+            ev_time: '12:31',
+            ev_loc: 'Brooklyn'
+        },
+        {
+            ev_type: 'Arsent',
+            ev_time: '20:15',
+            ev_loc: 'Manhatten'
+        }
+    ]   
 }
 
-const getReportById = async (id) => {
-    axios.get('')//TODO Add security API
-  .then(response => {
-
-    return response.data; 
-  })
-  .catch(error => {
-    throw error("can't get reports");
-  });
-}
-
-const parseReports = (reportsJSON) => {
-    reportsJSON = JSON.parse(reportsJSON);
-    let reportsToBackup = [];
-    reportsJSON = reportsJSON.reports.map(report => {
-        reportsToBackup.push([report.ev_type, report.ev_time, report.ev_report_time, report.reporter_id, report.ev_loc, report.report_id]);
-        return {"report_id": report.report_id,"ev_type": report.ev_type, "ev_time": report.ev_time, "ev_loc": report.ev_loc};
-    });
-    backupReports(reportsToBackup);
-    return reports;
-}
-
-const backupReports = (reportsArr) => {
-    const upsertSql = format('INSERT INTO t_events (ev_type, ev_time,ev_report_time, reporter_id, ev_loc, report_id)' +
-     'VALUES %L ON CONFLICT ON report_id' + 
-    'DO UPDATE SET ev_type=EXLUDED.ev_type, ev_time=EXLUDED.ev_time, ev_report_time=EXLUDED.ev_report_time, ev_loc=EXLUDED.ev_loc, reporter_id=EXLUDED.reporter_id;', reportsArr); 
-
-    pool.query(upsertSql).then(res => { return res.rows[0]; });
-}
-
-module.exports = {getReports, getReportById};
+module.exports = {
+    getAll,
+};
